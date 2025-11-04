87140ded7ee404d81d690c8f7e1f6704fa7c8630
Vishal Singh Baraiya <142595758+TheVixhal@users.noreply.github.com>
2025-11-04 13:33:57 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Momo+Trust+Display&display=swap" rel="stylesheet" />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}>
+      <body className="antialiased"}>
         {children}
         <Analytics />
       </body>
